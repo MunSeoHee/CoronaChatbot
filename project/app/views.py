4ac67860e4fea00e42ecf7a0982aa6f8a2a1b515
BbakSsang <alexpark216@gmail.com>
2020-07-25 02:03:41 +0900

    sy5

--- project/app/views.py
+++ project/app/views.py
@@ -90,6 +90,7 @@ def message(request):
 
 
 
+
         #sdaasdasd
         return JsonResponse({
   "version": "2.0",
@@ -100,59 +101,38 @@ def message(request):
           "type": "basicCard",
           "items": [
             {
-              "title": "보물상자",
-              "description": "보물상자 안에는 뭐가 있을까",
-              "thumbnail": {
-                "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
-              },
+              "title": "약국",
+              "description": "근처약국을 알려드려요",
+             
               "buttons": [
                 {
-                  "action": "message",
-                  "label": "열어보기",
-                  "messageText": "짜잔! 우리가 찾던 보물입니다"
-                },
-                {
                   "action":  "webLink",
-                  "label": "구경하기",
-                  "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
+                  "label": "지도보기",
+                  "webLinkUrl": "https://m.map.naver.com/search2/search.nhn?query="+addr[0]
                 }
               ]
             },
-            {
-              "title": "보물상자2",
-              "description": "보물상자2 안에는 뭐가 있을까",
-              "thumbnail": {
-                "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
-              },
+             {
+              "title": "약국",
+              "description": "근처약국을 알려드려요",
+             
               "buttons": [
                 {
-                  "action": "message",
-                  "label": "열어보기",
-                  "messageText": "짜잔! 우리가 찾던 보물입니다"
-                },
-                {
                   "action":  "webLink",
-                  "label": "구경하기",
-                  "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
+                  "label": "지도보기",
+                  "webLinkUrl": "https://m.map.naver.com/search2/search.nhn?query="+addr[1]
                 }
               ]
             },
-            {
-              "title": "보물상자3",
-              "description": "보물상자3 안에는 뭐가 있을까",
-              "thumbnail": {
-                "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
-              },
+             {
+              "title": "약국",
+              "description": "근처약국을 알려드려요",
+             
               "buttons": [
                 {
-                  "action": "message",
-                  "label": "열어보기",
-                  "messageText": "짜잔! 우리가 찾던 보물입니다"
-                },
-                {
                   "action":  "webLink",
-                  "label": "구경하기",
-                  "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
+                  "label": "지도보기",
+                  "webLinkUrl": "https://m.map.naver.com/search2/search.nhn?query="+addr[2]
                 }
               ]
             }
