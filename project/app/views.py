983a2dc42ba2f550b1fa5358abfda0d94807a43a
ToA <nansh9815@naver.com>
2020-07-24 21:27:43 +0900

    chatbot

--- project/app/views.py
+++ project/app/views.py
@@ -12,18 +12,20 @@ def info(request):
     answer = ((request.body).decode('utf-8'))
     return_json_str = json.loads(answer)
     return_str = return_json_str['userRequest']['utterance']
-    return JsonResponse({
-      "version": "2.0",
-    "template": {
-        "outputs": [
-            {
-                "simpleText": {
-                    "text": "간단한 텍스트 요소입니다."
+    
+    if return_str == "블록" :
+      return JsonResponse({
+          "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": return_json_str['userRequest']['block']['id']
+                    }
                 }
-            }
-        ]
-    }
-    })
+            ]
+        }
+        })
 @csrf_exempt
 def message(request):
     answer = ((request.body).decode('utf-8'))
