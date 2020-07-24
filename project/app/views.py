ed4b8327bb4e02618bfb03d826005a170eb29dce
ToA <nansh9815@naver.com>
2020-07-24 22:45:50 +0900

    d

--- project/app/views.py
+++ project/app/views.py
@@ -17,7 +17,10 @@ def message(request):
     
     if return_str == '확진자 정보' :
       id = return_json_str["userRequest"]["user"]["id"]
-      
+      obj, create = User.objects.get_or_create(
+        id = id,
+        location = '확진자 정보'
+      )
       return JsonResponse({
         "version": "2.0",
           "template": {
